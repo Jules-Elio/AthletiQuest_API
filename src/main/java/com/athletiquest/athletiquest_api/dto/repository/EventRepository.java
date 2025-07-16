ee843a8f2688cc4130b307654f5fad3448c4b66f
Sclus <julesmahe.mahe@gmail.com>
2025-07-16 16:01:52 +0200

    refactor: reformat

--- src/main/java/com/athletiquest/athletiquest_api/dto/repository/EventRepository.java
+++ src/main/java/com/athletiquest/athletiquest_api/dto/repository/EventRepository.java
@@ -15,7 +15,7 @@ public interface EventRepository extends JpaRepository<Event, Long> {
 
     List<Event> findByOwner(User owner);
 
-    @Query(value = "SELECT * FROM event WHERE ST_DWithin(event.coordinates, ST_SetSRID(ST_MakePoint(?1, ?2), 4326), ?3) ",
+    @Query(value = "SELECT * FROM event WHERE ST_DWithin(coordinates, ST_SetSRID(ST_MakePoint(?1, ?2), 4326), ?3)",
            nativeQuery = true)
     List<Event> findWithinDistance(double longitude, double latitude, double radius);
 }
